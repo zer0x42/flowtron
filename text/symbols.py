a6753d3cec631c4dece5e5e789713263b896d34a
zer0x42 <mcnielsen4270@gmail.com>
2020-07-15 15:41:04 +0200

    local changes

--- text/symbols.py
+++ text/symbols.py
@@ -11,7 +11,7 @@ _math = '#%&*+-/[]()'
 _special = '_@©°½—₩€$'
 _accented = 'áçéêëñöøćž'
 _numbers = '0123456789'
-_letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'
+_letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyzæøå'
 
 # Prepend "@" to ARPAbet symbols to ensure uniqueness (some are the same as uppercase letters):
 _arpabet = ['@' + s for s in cmudict.valid_symbols]
